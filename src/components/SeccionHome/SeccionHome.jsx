1d46d5134e65c3916aa5b60b1ea0a758c8255c6e
alexander windecker <waa_alex@hotmail.com>
2022-10-31 14:43:17 -0300

    borre comentarios

--- src/components/SeccionHome/SeccionHome.jsx
+++ src/components/SeccionHome/SeccionHome.jsx
@@ -1,27 +1,26 @@
 import React from "react";
-
-import "./SeccionHome.css"
-import {Link} from "react-router-dom"
+import "./SeccionHome.css";
+import { Link } from "react-router-dom";
 
 function SeccionHome() {
   return (
     <div>
-        <div id="presentacion" className="section">
-      <div className="contain">
-        <div className="img-container" data-aos="flip-left"></div>
-        <div className="texto">
-          <h1>
-            Nuevos Rumbos <span className="pastel">Pasteleria</span>
-          </h1>
-          <p>
-            Somos una empresa familiar dedicada a la pasteleria con mas de 10
-            años de experiencia. Nos destacamos por el amor dedicado a nuestro
-            trabajo con el objetivo de crear momentos especiales para nuestros
-            clientes. Los invito a acompañarnos a descubrir las dulzuras para
-            compartir.
-          </p>
+      <div id="presentacion" className="section">
+        <div className="contain">
+          <div className="img-container" data-aos="flip-left"></div>
+          <div className="texto">
+            <h1>
+              Nuevos Rumbos <span className="pastel">Pasteleria</span>
+            </h1>
+            <p>
+              Somos una empresa familiar dedicada a la pasteleria con mas de 10
+              años de experiencia. Nos destacamos por el amor dedicado a nuestro
+              trabajo con el objetivo de crear momentos especiales para nuestros
+              clientes. Los invito a acompañarnos a descubrir las dulzuras para
+              compartir.
+            </p>
+          </div>
         </div>
-      </div>
       </div>
       <main id="nuestros-servicios" className="main">
         <div className="container">
@@ -31,12 +30,12 @@ function SeccionHome() {
               <h3>Pasteleria Artesanal</h3>
               <p>
                 Pasteleria elaborada y producida por pasteleros profesionales.
-                En la <span className="fw-bold">reposteria artesanal</span> todos
-                los productos se elaboran a mano y normalmente bajo los
+                En la <span className="fw-bold">reposteria artesanal</span>{" "}
+                todos los productos se elaboran a mano y normalmente bajo los
                 seguimientos de las recetas mas tradicionales con ingredientes
                 frescos y naturales.
               </p>
-              <Link className="nav-link" >
+              <Link className="nav-link" to="Nosotros">
                 <button className="info-button">+ Info</button>
               </Link>
             </div>
@@ -50,22 +49,22 @@ function SeccionHome() {
                 que sin importar lo pequeño o grande que sea tu evento, seguro
                 podemos armar una seleccion acorde a cada ocasion.
               </p>
-             
-              <Link className="nav-link" >
+
+              <Link className="nav-link" to="Nosotros">
                 <button className="info-button">+ Info</button>
               </Link>
-              
             </div>
 
             <div className="carta3">
               <h3>Pasteleria Diseños</h3>
               <p>
-                En <span className="fw-bold">Pastelería de diseño</span> ofrecemos
-                tortas de vanguardia, decoradas con las últimas tendencias para
-                reflejar la elegancia y sofisticación de su próximo evento. Como
-                asi tambien personalizadas y exclusivas al estilo del cliente
+                En <span className="fw-bold">Pastelería de diseño</span>{" "}
+                ofrecemos tortas de vanguardia, decoradas con las últimas
+                tendencias para reflejar la elegancia y sofisticación de su
+                próximo evento. Como asi tambien personalizadas y exclusivas al
+                estilo del cliente
               </p>
-              <Link className="nav-link" >
+              <Link className="nav-link" to="Nosotros">
                 <button className="info-button">+ Info</button>
               </Link>
             </div>
--- src/components/context/CartContext.jsx
+++ src/components/context/CartContext.jsx
@@ -7,13 +7,11 @@ const { Provider } = CartContext;
 
 const MyProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
-  //Funcion en la que indica si el producto agregado al carrito ya existe o no en el cart con el metodo "some"
+
   const isInCart = (id) => {
     return cart.some((x) => x.id === id);
   };
-  //Funcion en la que indica si el producto agregado al carrito ya existe o no en el cart con el metodo "some"
 
-  //Funcion agregar producto al carrito, sin pisar los productos ya existentes en el cart
   const addItem = (data, qty) => {
     const newItem = { ...data, qty };
     if (isInCart(newItem.id)) {
@@ -26,31 +24,22 @@ const MyProvider = ({ children }) => {
       setCart([...cart, newItem]);
     }
   };
-  //Funcion agregar producto al carrito, sin pisar los productos ya existentes en el cart
 
-  //Funcion que borra todos los productos de nuestro carrito.
   const emptyCart = () => {
     return setCart([]);
   };
-  //Funcion que borra todos los productos de nuestro carrito.
 
-  //Funcion para borrar un producto especifico del carrito.
   const deleteItem = (id) => {
     return setCart(cart.filter((x) => x.id !== id));
   };
-  //Funcion para borrar un producto especifico del carrito.
 
-  //funcion que retorna cantidad de unidades que tiene nuestro carrito
   const getItemQty = () => {
     return cart.reduce((acc, x) => (acc += x.qty), 0);
   };
-  //funcion que retorna cantidad de unidades que tiene nuestro carrito
 
-  //funcion que retorna el precio total del carrito
   const getItemPrice = () => {
     return cart.reduce((acc, x) => (acc += x.price * x.qty), 0);
   };
-  //funcion que retorna el precio total del carrito
 
   return (
     <Provider
